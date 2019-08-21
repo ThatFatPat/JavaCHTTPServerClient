e01a71926b6c1b908f9cde84d466045c659b60de
ThatFatPat <idoshav@gmail.com>
2019-08-21 16:33:15 +0300

    Add send_data function

--- C_Helper/c_helper.c
+++ C_Helper/c_helper.c
@@ -1,9 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
 #include "c_helper.h"
 
 void error(char *msg)
 {
     perror(msg);
     exit(1);
+}
+
+void print_msg(char* msg)
+{
+    printf("%s\n", msg);
+}
+
+int send_data(int sockfd, const char* msg, size_t size){
+    size_t sent = 0;
+    int bytes = 0;
+    int ret = 0;
+    while (sent < size){
+        bytes = write(sockfd, msg+sent, size-sent);
+        if (bytes < 0){
+            ret = bytes;
+            break;
+        }
+        if (bytes == 0){
+            break;
+        }
+        sent+=bytes;
+    }
+    if (bytes >= 0){
+        return size;
+    }
+    return ret;
 }
--- C_Helper/c_helper.h
+++ C_Helper/c_helper.h
@@ -1,3 +1,6 @@
 #pragma once
+#include <stddef.h>
 
 void error(char *msg);
+void print_msg(char* msg);
+int send_data(int sockfd, const char* data, size_t size);
